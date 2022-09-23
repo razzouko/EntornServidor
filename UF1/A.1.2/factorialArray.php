67c561594c58d177a53b5563572d72f7f1c6958c
razzouko <100167727+razzouko@users.noreply.github.com>
2022-09-23 19:03:51 +0200

    a.1.2

---
+++ UF1/A.1.2/factorialArray.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+        #Funcio que fa el factorial de qualsevol número
+         
+        function factorial($numero){
+            if($numero < 0) 
+                return false;
+            if ($numero == 1) 
+                return 1;
+            else 
+                return $numero * factorial($numero -1);
+        }
+    
+        function factorialArray($array) : array | bool{
+
+            if(  is_array($array) != true ) 
+                return false;
+      
+            for( $i = 0 ; $i < count($array) ; $i++ ){
+                if (is_numeric($array[$i]) != true)
+                    return false;
+                $array[$i] = factorial($array[$i]);
+                if ($array[$i] == false)
+                    return false;
+            }
+            return $array;
+        }
+        
+        
+    ?>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ UF1/A.1.2/multidimensionals.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+
+            creaMatriu(int $n) : array {
+
+                $matriu = []
+                $fila = 0;
+                $col = 0;
+                $asterisc = 0;
+                for( $i = 0; $i <= $n ; $i++){
+
+                    for($j = 0 ; $j < $n ; $i++ ){
+
+                        if ( $asterisc == $col)
+                            
+
+                    }
+
+                }
+
+
+
+            }
+
+
+     
+
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
