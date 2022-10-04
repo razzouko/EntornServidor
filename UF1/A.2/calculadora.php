a72ef2d31c06b219e5175a6f6d56d75c4ce1784c
razzouko <orazzouk8@boscdelacoma.cat>
2022-10-04 23:17:22 +0200

    commit final :)

--- UF1/A.2/calculadora.php
+++ UF1/A.2/calculadora.php
@@ -8,18 +8,8 @@
 <body>
     <div class="container"> 
         <form action="" name="calc" class="calculator" method = "post">
-            <input type="text" class="value"  name="screen" readonly value="
-            <?php
-            
-            if(isset($_POST["ultimValor"])){
-                $ultimValor = $_POST["ultimValor"];
-            }else{
-                $ultimValor = "";
-            } 
-            echo ferCalcul(); 
-            $ultimValor = $_POST["btn"]; 
-            ?>"/>
-            <input type="hidden" name="ultimValor" value=" <?php echo $ultimValor; ?> ">
+            <input type="text" class="value"  name="screen" readonly value="<?php   echo ferCalcul()?>"/>
+            <input type="hidden" name="lastval" value="<?php echo (isset($_POST["btn"])) ? $_POST["btn"] : ""; //input amagat on guardo l'últim valor que s'ha clicat?>"> 
             <span class="num"><input type ="submit" value="(" name ="btn"></span>
             <span class="num"><input type ="submit" value=")" name ="btn"></span>
             <span class="num"><input type ="submit" value="sin" name ="btn" ></span>
@@ -46,34 +36,42 @@
 
         <?php 
         
+            /**
+            * funció ferCalcul() verifica , construeix i calcula l'operació proposada
+            * @param {} 
+            * @returns {}
+            */
             function ferCalcul(){
 
-                if($ultimValor == ""){
-                    return "noup";
-                }else{
-                    return "funciona";
-                }
-
-                if( !isset($_POST["btn"]) || !isset($_POST["screen"]))
+                if(!isset($_POST["btn"]) || !isset($_POST["screen"]))
                     return"";
+
+                if(preg_match('/^[\$]$/' , $_POST["screen"]))
+                    return;
 
                 if($_POST["btn"] == "C")
                     return;
     
-                if($_POST["btn"] != "="){
-                    if( $ultim )
-                        if(preg_match('/^[\+|\-|\*|\/]$/' , substr($_POST["screen"] , -1)) 
-                            && preg_match('/^[\+|\-|\*|\/]$/' ,$_POST["btn"])){
-                                return substr_replace($_POST["screen"] , $_POST["btn"] , -1 , 1);
-                            }elseif($_POST["btn"] == "sin" || $_POST["btn"] == "cos"){
-                                return $_POST["screen"] . $_POST["btn"] . "(";
-                            }
+                if($_POST["btn"] != "=" && $_POST["lastval"] != "="){
+                    if(preg_match('/^[\+|\-|\*|\/]$/' , substr($_POST["screen"] , -1)) 
+                    && preg_match('/^[\+|\-|\*|\/]$/' ,$_POST["btn"])){
+                        return substr_replace($_POST["screen"] , $_POST["btn"] , -1 , 1);
+                    }elseif($_POST["btn"] == "sin" || $_POST["btn"] == "cos"){
+                        return $_POST["screen"] . $_POST["btn"] . "(";
+                    }else
                         return $_POST["screen"] . $_POST["btn"];
+                }elseif($_POST["lastval"] == "=" && $_POST["btn"] != "=" ){
+                    if($_POST["btn"] == "sin")
+                        return "sin(" . $_POST["screen"] . ")";
+                    elseif($_POST["btn"] == "cos")
+                        return "cos(" . $_POST["screen"] . ")";
+                    else
+                        return $_POST["btn"]; 
                 }
 
-                // protegir el codi de l'eval
                 if($_POST["btn"] == "="){
-                    $operacio = $_POST["screen"];
+                    $operacio = comprovarParentesis();
+                    $operacio = comprovarMultiplicar($operacio);
                     try{
                         eval("\$resultat = round(" . $operacio . " , 4);");
                         return $resultat;
@@ -83,7 +81,60 @@
                         return "Error";
                     }
                     return $resultat;
+                  
                 }      
+            }
+
+            /**
+            * funció comprovarParentesis() comprova que els parèntesis estiguin correctament posat i sino arregla l'operació
+            * 
+            * @param {} 
+            * @returns {}
+            */
+            function comprovarParentesis(){
+                if(!preg_match('/\(/', $_POST["screen"]))
+                    return $_POST["screen"];
+
+                $stack = [];
+                $operacio = $_POST["screen"];
+                for($i = 0 ; $i < strlen($operacio) ; $i++){
+                    if($operacio[$i] == "(")
+                        $stack[] = $operacio[$i];
+
+                    if($operacio[$i] == ")")
+                        array_pop($stack);
+                }
+
+                for($i = 0 ; $i < count($stack) ; $i++){
+                    
+                    $operacio .= ')';
+
+                }
+                return $operacio;
+            }
+
+            /**
+            * funció comprovarMultiplicar() pasada una operacio on es dona el cas que no s'especifica operant, entre un nombre i un parèntesis
+            * o sin o cos, arregala l'operació interpretant l'operació com una multiplicació.
+            *   
+            * @param {String} 
+            * @returns {String}
+            */
+            function comprovarMultiplicar( string $operacio) : string{
+
+                for($i = 1 ; $i < strlen($operacio) ; $i++){
+                    
+                        if($operacio[$i] == "(" )
+                            if(is_numeric($operacio[$i - 1]))
+                               $operacio = substr_replace($operacio , "*" , $i , 0);
+                        
+                        if($operacio[$i] == "s" || $operacio[$i] == "c")
+                            if(is_numeric($operacio[$i - 1]))
+                                $operacio = substr_replace($operacio , "*" , $i , 0);
+
+                }
+
+                return $operacio;
             }
 
         ?>
--- UF1/A.2/proves.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-    echo substr_replace("hola" , "u" , -1 ,1)
-
-?>
